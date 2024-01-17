ffd74c9bce024b1a8c782ffafce8eaa514423d23
pavan-3000 <pavansingh3000@gmail.com>
2024-01-17 15:54:13 +0530

    requirment added

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "mlflow"
+    version=0.0.0.0,
+    author = "pavan",
+    author_email="pavansingh3000@gmail.com",
+    packages=setuptools.find_packages()
+)
--- templet.py
+++ templet.py
@@ -18,7 +18,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/predict_pipeline.py",
     f"src/{project_name}/logger.py",
     f"src/{project_name}/exception.py",
-    f"src/{project_name}/utile.py"
+    f"src/{project_name}/utile.py",
     "requirment.txt",
     "setup.py"
     
